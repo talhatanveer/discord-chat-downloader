08e7aea42bd084a4d886172b2d0a7f0cfa062c17
Talha Tanveer <t.tanveer@icloud.com>
2022-06-20 04:41:45 -0500

    a working downloader

--- api.py
+++ api.py
@@ -3,9 +3,11 @@ import requests, json
 version = 'v9'
 endpoint = f'https://discord.com/api/{version}'
 
-def discord_api(endpoint, token, params = {}):
+def discord_api(path, token, params = {}):
     try:
-        r = requests.get(endpoint, params=params)
+        r = requests.get(f'{endpoint}{path}', params=params, headers={
+            'Authorization':token
+        })
         return r.json()
     except Exception as e:
         print("The following error occured: ")
--- discord_downloader.py
+++ discord_downloader.py
@@ -1,8 +1,10 @@
-import requests, json, os
+import os, json
+from time import sleep
 from api import discord_api
 
 if not os.path.exists(".token"):
     print("No Authorization Token Found!")
+    print("See README.md for how to find your authorization token")
     token = input("Enter Authorization Token: ")
 
     with open(".token", "w+") as f:
@@ -16,6 +18,7 @@ else:
     with open(".token", "r") as f:
         token = f.readline()
 
+print("Retrieving all your channels...")
 channels = discord_api("/users/@me/channels", token)
 
 if not channels:
@@ -23,7 +26,59 @@ if not channels:
     print("Please make sure you Authorization Token is correct")
     exit(0)
 
-user_id = input("Enter the Username of the chat you want to download: ")
+username = ''
 
-def DownloadChat():
-    pass
+while True:
+    username = input("Enter Username (or 0 to exit): ")
+
+    if username == '0':
+        break
+
+    channel = [c for c in channels if c['recipients'][0]['username'] == username]
+
+    if len(channel) == 0:
+        print("User not found! Usernames are case-sensitive.")
+    else:
+        channel = channel[0]
+        channel_id = channel['id']
+        file = input(f'Enter save file name ({username}.json by default): ')
+        
+        if file.strip() == '':
+            file = f'{username}.json'
+        
+        messages = []
+
+        last_message_id = 0
+        counter = 0
+
+        while True:
+            print(f'Fetching Messages ({counter} total)')
+
+            msgs = discord_api(f'/channels/{channel_id}/messages', token, params={
+                'after': last_message_id,
+                'limit': 100 # max limit supported by Discord API
+            })
+
+            if not msgs:
+                print('Error while retrieving messages')
+                exit(0)
+
+            ## Rate Limiting Issue
+
+            if "retry_after" in msgs:
+                print("Rate limited ... waiting 5 seconds before retrying")
+                sleep(5)
+            else:
+                messages.extend(msgs)
+                counter += len(msgs)
+
+                if len(msgs) < 100:
+                    break
+                else:
+                    last_message_id = msgs[len(msgs) - 1]['id']
+        
+        print(f"Fetched a total of {counter} messages")
+        with open(file, 'w+') as f:
+            f.write(json.dumps(messages, indent = 2))
+            f.close()
+        print('Messages Saved!')
